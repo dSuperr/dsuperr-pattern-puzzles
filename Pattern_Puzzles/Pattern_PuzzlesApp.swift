012871e59b64827e8bc4f650b2dc5d83b41c1b96
Daksh Singh <daksh.singh.01012012@gmail.com>
2021-12-12 21:28:13 +0530

    Initial Commit

---
+++ Pattern_Puzzles/Pattern_PuzzlesApp.swift
@@ -0,0 +1,17 @@
+//
+//  Pattern_PuzzlesApp.swift
+//  Pattern_Puzzles
+//
+//  Created by Daksh Singh on 12/12/21.
+//
+
+import SwiftUI
+
+@main
+struct Pattern_PuzzlesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
